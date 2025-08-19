58735e2c833d57b6450b4951ff6e8aa0f0fb4c00
MrQwest <128bit@128bit.xyz>
2025-08-20 00:59:11 +0300

    realy work text2image

--- CProGAN.py
+++ CProGAN.py
@@ -1,13 +1,8 @@
 """
-Implementation of ProGAN generator and discriminator with the key
-attributions from the paper. We have tried to make the implementation
-compact but a goal is also to keep it readable and understandable.
-Specifically the key points implemented are:
-
-1) Progressive growing (of model and layers)
-2) Minibatch std on Discriminator
-3) Normalization with PixelNorm
-4) Equalized Learning Rate (here I cheated and only did it on Conv layers)
+Implementation of Conditional ProGAN (C-ProGAN) with key attributes from the ProGAN paper.
+This implementation incorporates conditional embeddings to support class-conditional generation,
+while maintaining progressive growing, minibatch standard deviation, pixel normalization,
+and equalized learning rate for convolutional layers.
 """
 
 import torch
@@ -15,36 +10,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 from math import log2
 
-"""
-Factors is used in Discrmininator and Generator for how much
-the channels should be multiplied and expanded for each layer,
-so specifically the first 5 layers the channels stay the same,
-whereas when we increase the img_size (towards the later layers)
-we decrease the number of chanels by 1/2, 1/4, etc.
-"""
-factors = [1, 1, 1, 1, 1 / 2, 1 / 4, 1 / 8, 1 / 16, 1 / 32]
+# Channel scaling factors for progressive growing
+factors = [1, 1, 1, 1, 1/2, 1/4, 1/8, 1/16, 1/32]
 
 
 class WSConv2d(nn.Module):
     """
-    Weight scaled Conv2d (Equalized Learning Rate)
-    Note that input is multiplied rather than changing weights
-    this will have the same result.
-
-    Inspired and looked at:
-    https://github.com/nvnbny/progressive_growing_of_gans/blob/master/modelUtils.py
+    Weight-scaled Conv2d with Equalized Learning Rate.
+    Input scaling is used instead of weight scaling for equivalent effect.
     """
-
-    def __init__(
-            self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, gain=2
-    ):
-        super(WSConv2d, self).__init__()
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, gain=2):
+        super().__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
         self.scale = (gain / (in_channels * (kernel_size ** 2))) ** 0.5
         self.bias = self.conv.bias
         self.conv.bias = None
 
-        # initialize conv layer
+        # Initialize weights and biases
         nn.init.normal_(self.conv.weight)
         nn.init.zeros_(self.bias)
 
@@ -52,9 +34,31 @@ class WSConv2d(nn.Module):
         return self.conv(x * self.scale) + self.bias.view(1, self.bias.shape[0], 1, 1)
 
 
+class WSLinear(nn.Module):
+    """
+    Weight-scaled Linear layer with Equalized Learning Rate.
+    """
+    def __init__(self, in_features, out_features, gain=2):
+        super().__init__()
+        self.linear = nn.Linear(in_features, out_features)
+        self.scale = (gain / in_features) ** 0.5
+        self.bias = self.linear.bias
+        self.linear.bias = None
+
+        # Initialize weights and biases
+        nn.init.normal_(self.linear.weight)
+        nn.init.zeros_(self.bias)
+
+    def forward(self, x):
+        return self.linear(x * self.scale) + self.bias
+
+
 class PixelNorm(nn.Module):
+    """
+    Pixel-wise normalization layer.
+    """
     def __init__(self):
-        super(PixelNorm, self).__init__()
+        super().__init__()
         self.epsilon = 1e-8
 
     def forward(self, x):
@@ -62,8 +66,11 @@ class PixelNorm(nn.Module):
 
 
 class ConvBlock(nn.Module):
+    """
+    Convolutional block with two WSConv2d layers, LeakyReLU, and optional PixelNorm.
+    """
     def __init__(self, in_channels, out_channels, use_pixelnorm=True):
-        super(ConvBlock, self).__init__()
+        super().__init__()
         self.use_pn = use_pixelnorm
         self.conv1 = WSConv2d(in_channels, out_channels)
         self.conv2 = WSConv2d(out_channels, out_channels)
@@ -79,30 +86,26 @@ class ConvBlock(nn.Module):
 
 
 class Generator(nn.Module):
-    def __init__(self, z_dim, in_channels, img_channels=3):
-        super(Generator, self).__init__()
-
-        # initial takes 1x1 -> 4x4
+    """
+    Conditional ProGAN Generator with progressive growing and embedding support.
+    """
+    def __init__(self, z_dim, in_channels, img_channels=3, emb_size=768):
+        super().__init__()
+        # Initial block: 1x1 -> 4x4 with embedding
         self.initial = nn.Sequential(
             PixelNorm(),
-            nn.ConvTranspose2d(z_dim, in_channels, 4, 1, 0),
+            nn.ConvTranspose2d(z_dim + emb_size, in_channels, 4, 1, 0),
             nn.LeakyReLU(0.2),
             WSConv2d(in_channels, in_channels, kernel_size=3, stride=1, padding=1),
             nn.LeakyReLU(0.2),
             PixelNorm(),
         )
+        self.initial_rgb = WSConv2d(in_channels, img_channels, kernel_size=1, stride=1, padding=0)
+        self.prog_blocks = nn.ModuleList([])
+        self.rgb_layers = nn.ModuleList([self.initial_rgb])
 
-        self.initial_rgb = WSConv2d(
-            in_channels, img_channels, kernel_size=1, stride=1, padding=0
-        )
-        self.prog_blocks, self.rgb_layers = (
-            nn.ModuleList([]),
-            nn.ModuleList([self.initial_rgb]),
-        )
-
-        for i in range(
-                len(factors) - 1
-        ):  # -1 to prevent index error because of factors[i+1]
+        # Progressive blocks
+        for i in range(len(factors) - 1):
             conv_in_c = int(in_channels * factors[i])
             conv_out_c = int(in_channels * factors[i + 1])
             self.prog_blocks.append(ConvBlock(conv_in_c, conv_out_c))
@@ -111,10 +114,15 @@ class Generator(nn.Module):
             )
 
     def fade_in(self, alpha, upscaled, generated):
-        # alpha should be scalar within [0, 1], and upscale.shape == generated.shape
+        """
+        Fade-in mechanism for smooth transition between resolutions.
+        """
         return torch.tanh(alpha * generated + (1 - alpha) * upscaled)
 
-    def forward(self, x, alpha, steps):
+    def forward(self, x, labels, alpha, steps):
+        # Concatenate noise and embedding
+        embedding = labels.unsqueeze(2).unsqueeze(3)
+        x = torch.cat([x, embedding], dim=1)
         out = self.initial(x)
 
         if steps == 0:
@@ -124,90 +132,80 @@ class Generator(nn.Module):
             upscaled = F.interpolate(out, scale_factor=2, mode="nearest")
             out = self.prog_blocks[step](upscaled)
 
-        # The number of channels in upscale will stay the same, while
-        # out which has moved through prog_blocks might change. To ensure
-        # we can convert both to rgb we use different rgb_layers
-        # (steps-1) and steps for upscaled, out respectively
         final_upscaled = self.rgb_layers[steps - 1](upscaled)
         final_out = self.rgb_layers[steps](out)
         return self.fade_in(alpha, final_upscaled, final_out)
 
 
 class Discriminator(nn.Module):
-    def __init__(self, z_dim, in_channels, img_channels=3):
-        super(Discriminator, self).__init__()
-        self.prog_blocks, self.rgb_layers = nn.ModuleList([]), nn.ModuleList([])
+    """
+    Conditional ProGAN Discriminator with progressive growing and embedding support.
+    """
+    def __init__(self, z_dim, in_channels, img_channels=3, emb_size=768):
+        super().__init__()
+        self.prog_blocks = nn.ModuleList([])
+        self.rgb_layers = nn.ModuleList([])
+        self.emb_layers = nn.ModuleList([])
         self.leaky = nn.LeakyReLU(0.2)
 
-        # here we work back ways from factors because the discriminator
-        # should be mirrored from the generator. So the first prog_block and
-        # rgb layer we append will work for input size 1024x1024, then 512->256-> etc
+        # Progressive blocks in reverse order
         for i in range(len(factors) - 1, 0, -1):
             conv_in = int(in_channels * factors[i])
             conv_out = int(in_channels * factors[i - 1])
             self.prog_blocks.append(ConvBlock(conv_in, conv_out, use_pixelnorm=False))
             self.rgb_layers.append(
-                WSConv2d(img_channels, conv_in, kernel_size=1, stride=1, padding=0)
+                WSConv2d(img_channels + 1, conv_in, kernel_size=1, stride=1, padding=0)
             )
+            res = 2 ** (i + 2)
+            self.emb_layers.append(WSLinear(emb_size, res * res))
 
-        # perhaps confusing name "initial_rgb" this is just the RGB layer for 4x4 input size
-        # did this to "mirror" the generator initial_rgb
-        self.initial_rgb = WSConv2d(
-            img_channels, in_channels, kernel_size=1, stride=1, padding=0
-        )
+        # Initial RGB layer for 4x4 input
+        self.initial_rgb = WSConv2d(img_channels + 1, in_channels, kernel_size=1, stride=1, padding=0)
         self.rgb_layers.append(self.initial_rgb)
-        self.avg_pool = nn.AvgPool2d(
-            kernel_size=2, stride=2
-        )  # down sampling using avg pool
+        self.emb_layers.append(WSLinear(emb_size, 4 * 4))
+        self.avg_pool = nn.AvgPool2d(kernel_size=2, stride=2)
 
-        # this is the block for 4x4 input size
+        # Final block for 4x4 input
         self.final_block = nn.Sequential(
-            # +1 to in_channels because we concatenate from MiniBatch std
             WSConv2d(in_channels + 1, in_channels, kernel_size=3, padding=1),
             nn.LeakyReLU(0.2),
             WSConv2d(in_channels, in_channels, kernel_size=4, padding=0, stride=1),
             nn.LeakyReLU(0.2),
-            WSConv2d(
-                in_channels, 1, kernel_size=1, padding=0, stride=1
-            ),  # we use this instead of linear layer
+            WSConv2d(in_channels, 1, kernel_size=1, padding=0, stride=1),
         )
 
     def fade_in(self, alpha, downscaled, out):
-        """Used to fade in downscaled using avg pooling and output from CNN"""
-        # alpha should be scalar within [0, 1], and upscale.shape == generated.shape
+        """
+        Fade-in mechanism for smooth transition between resolutions.
+        """
         return alpha * out + (1 - alpha) * downscaled
 
     def minibatch_std(self, x):
-        batch_statistics = (
-            torch.std(x, dim=0).mean().repeat(x.shape[0], 1, x.shape[2], x.shape[3])
-        )
-        # we take the std for each example (across all channels, and pixels) then we repeat it
-        # for a single channel and concatenate it with the image. In this way the discriminator
-        # will get information about the variation in the batch/image
+        """
+        Minibatch standard deviation layer for batch diversity.
+        """
+        batch_statistics = torch.std(x, dim=0).mean().repeat(x.shape[0], 1, x.shape[2], x.shape[3])
         return torch.cat([x, batch_statistics], dim=1)
 
-    def forward(self, x, alpha, steps):
-        # where we should start in the list of prog_blocks, maybe a bit confusing but
-        # the last is for the 4x4. So example let's say steps=1, then we should start
-        # at the second to last because input_size will be 8x8. If steps==0 we just
-        # use the final block
+    def forward(self, x, labels, alpha, steps):
         cur_step = len(self.prog_blocks) - steps
+        res = 4 * (2 ** steps)
+        # Transform embedding to spatial dimensions
+        embedding = self.emb_layers[cur_step](labels).view(labels.shape[0], 1, res, res)
+        x_cat = torch.cat([x, embedding], dim=1)
+        out = self.leaky(self.rgb_layers[cur_step](x_cat))
 
-        # convert from rgb as initial step, this will depend on
-        # the image size (each will have it's on rgb layer)
-        out = self.leaky(self.rgb_layers[cur_step](x))
-
-        if steps == 0:  # i.e, image is 4x4
+        if steps == 0:
             out = self.minibatch_std(out)
             return self.final_block(out).view(out.shape[0], -1)
 
-        # because prog_blocks might change the channels, for down scale we use rgb_layer
-        # from previous/smaller size which in our case correlates to +1 in the indexing
-        downscaled = self.leaky(self.rgb_layers[cur_step + 1](self.avg_pool(x)))
+        # Downscaled path with embedding
+        embedding_low = self.avg_pool(embedding)
+        x_low = self.avg_pool(x)
+        x_low_cat = torch.cat([x_low, embedding_low], dim=1)
+        downscaled = self.leaky(self.rgb_layers[cur_step + 1](x_low_cat))
         out = self.avg_pool(self.prog_blocks[cur_step](out))
 
-        # the fade_in is done first between the downscaled and the input
-        # this is opposite from the generator
         out = self.fade_in(alpha, downscaled, out)
 
         for step in range(cur_step + 1, len(self.prog_blocks)):
@@ -216,3 +214,15 @@ class Discriminator(nn.Module):
 
         out = self.minibatch_std(out)
         return self.final_block(out).view(out.shape[0], -1)
+
+
+def initialize_weights(model):
+    """
+    Initialize weights according to the ProGAN methodology.
+    """
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.Linear)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+        elif isinstance(m, nn.BatchNorm2d):
+            nn.init.normal_(m.weight.data, 1.0, 0.02)
+            nn.init.constant_(m.bias.data, 0.0)
